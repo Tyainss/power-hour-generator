827ff08c4dba6cfdeb7968665c5a0a0cbf2bec1b
Rui Pereira <ruiarpereira15@gmail.com>
2024-12-10 23:56:22 +0000

    :spakles: create config_manager

---
+++ utils/config_manager.py
@@ -0,0 +1,23 @@
+import json
+
+class ConfigManager:
+    CONFIG_PATH_DEFAULT = 'config.json'
+    
+    def __init__(self, config_path = CONFIG_PATH_DEFAULT) -> None:
+        self.config = self.load_json(config_path)
+
+        self.SOUND_CLIPS_PATH = self.config['SOUND_CLIPS_PATH']
+        self.DEFAULT_SONG_DURATION = self.config['DEFAULT_SONG_DURATION']
+        self.DEFAULT_NUMBER_SONGS = self.config['DEFAULT_NUMBER_SONGS']
+        self.MAX_NUMBER_SONGS = self.config['MAX_NUMBER_SONGS']
+
+    def load_json(self, path: str, encoding: str = 'utf-8') -> dict:
+        try:
+            with open(path, 'r', encoding=encoding) as f:
+                return json.load(f)
+        except FileNotFoundError:
+            logger.error(f"Error: The file {path} does not exist.")
+            return {}
+        except json.JSONDecodeError:
+            logger.error(f"Error: The file {path} is not a valid JSON.")
+            return {}
